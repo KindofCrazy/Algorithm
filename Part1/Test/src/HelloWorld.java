1ee7ca71de7330a65c922b94fbb6b174854f6e19
Yang Ke <1652467250@qq.com>
2023-09-10 11:56:16 +0800

    Update

--- Part1/Test/src/HelloWorld.java
+++ Part1/Test/src/HelloWorld.java
@@ -1,10 +1,7 @@
-import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.StdRandom;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        StdOut.println("Hello " + args[0] +" and " + args[1]);
-        StdOut.println("Goodbye " + args[1] + " and " + args[0]);
+        StdOut.println("Hello, World");
     }
 }
